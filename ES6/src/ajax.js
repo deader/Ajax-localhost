c5784b420389241c96571bfa12f947448289ba0b
deader <deader25@ukr.net>
2016-11-23 14:52:23 +0200

    Изучение Ajax на localhost (stage 3)

--- ES6/src/ajax.js
+++ ES6/src/ajax.js
@@ -29,39 +29,57 @@ CreateRequest();
 $(function () {
     $('#getForecast').click(function () {
         console.log('Клик по кнопке');
-        $.ajax({
-            type: "GET",
-            url: "http://t2.alexlans.com/test/src/promises.js",
-            dataType: "script"
-        });
+        // $.ajax({
+        //     type: "GET",
+        //     url: "/test/src/promises.js",
+        //     dataType: "script"
+        // });
+
+// Запрос без параметров
 
         $.ajax({
             type: "GET",
-            url: "http://t2.alexlans.com/test/getForecast.js",
+            url: "/test/getForecast.js",
             dataType: "script",
             success: success
         });
 
-        $.ajax({
-            type: "GET",
-            url: "http://t2.alexlans.com/test/getForecast.json",
-            dataType: "json",
-            success: 
-            function success3(data) {
-                console.log('Success3!!!');
-                document.write(data);
-            }
-        });
+        // $.ajax({
+        //     type: "GET",
+        //     url: "/test/ajax-tunel.js",
+        //     dataType: "script"
+        // });
 
-        var data = { city: "Васюки", date: "20120318" };
-        $.get("http://t2.alexlans.com/test/getForecast.json", data, success2, "json");
+        // $.ajax({
+        //     type: "GET",
+        //     url: "/test/getForecast.json",
+        //     dataType: "json",
+        //     success:
+        //     function success3(data) {
+        //         console.log('Success3!!!');
+        //         document.write(data);
+        //     }
+        // });
 
         function success() {
             console.log('Success!!!');
             var forecast = forecastData.city + ", прогноз на " + forecastData.date;
             forecast += ": " + forecastData.forecast + ". Максимальная температура: " + forecastData.maxTemp + "C";
-            console.log(forecast);
+            console.log('А вот и сам forcast: ', forecast);
         }
+
+// Запрос с параметрами
+
+        var data = { city: "Васюки", date: "20120318" };
+        $.get("/test/getForecast.json", data, success4, "json");
+
+        function success4(Data) {
+            console.log('Success!!!');
+            var forecast = Data.city + ", прогноз на " + Data.date;
+            forecast += ": " + Data.forecast + ". Максимальная температура: " + Data.maxTemp + "C";
+            console.log('А вот и сам forcast: ', forecast);
+        }
+
 
         function success2(html) {
             console.log('Success2!!!');
@@ -71,3 +89,65 @@ $(function () {
     });
 
 });
+
+
+
+
+// Новый тест 2 разные типы запросов
+
+
+function SendGet() {
+    //отправляю GET запрос и получаю ответ
+    $$a({
+        type: 'get',//тип запроса: get,post либо head
+        url: '/test/ajax.php',//url адрес файла обработчика
+        data: { 'q': '1' },//параметры запроса
+        response: 'text',//тип возвращаемого ответа text либо xml
+        success: function (data) {//возвращаемый результат от сервера
+            $$('result', $$('result').innerHTML + '<br />' + data);
+        }
+    });
+}
+
+function SendPost() {
+    //отправляю POST запрос и получаю ответ
+    $$a({
+        type: 'post',//тип запроса: get,post либо head
+        url: '/test/ajax.php',//url адрес файла обработчика
+        data: { 'z': '1' },//параметры запроса
+        response: 'text',//тип возвращаемого ответа text либо xml
+        success: function (data) {//возвращаемый результат от сервера
+            $$('result', $$('result').innerHTML + '<br />' + data);
+        }
+    });
+}
+
+function SendHead() {
+    //отправляю HEAD запрос и получаю заголовок
+    $$a({
+        type: 'head',//тип запроса: get,post либо head
+        url: '/test/ajax.php',//url адрес файла обработчика
+        response: 'text',//тип возвращаемого ответа text либо xml
+        success: function (data) {//возвращаемый результат от сервера
+            $$('result', $$('result').innerHTML + '<br />' + data);
+        }
+    });
+}
+
+
+// Новый тест 3 с видеоурока https://www.youtube.com/watch?v=aw-wOOatKNI
+function ajax_test_3() {
+    let request = CreateRequest();
+    let url = location.href;
+
+    request.onreadystatechange = function () {
+        // только при состоянии 'complete'
+        if (request.readyState == 4) {
+
+        }
+    }
+    // Запрос на сервер
+    request.open('GET', url, false);
+    request.send(null);
+    alert(request.responseText);
+}
