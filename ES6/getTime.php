76f7a62654e646b67b94ea4a6cffde596182af8d
deader <deader25@ukr.net>
2016-11-24 17:27:38 +0200

    Продолжение изучения Ajax по видеоуроку 1

--- ES6/getTime.php
+++ ES6/getTime.php
@@ -5,6 +5,7 @@ header("Content-type: text/plain; charset=utf-8");
 header("Cache-Control: no-store");
 
 // Текущее время
-echo date("H:i:s");
+$time = time() - 1 * 60 * 60; // секунды
+echo date('H:i:s', $time); 
 
 ?>
